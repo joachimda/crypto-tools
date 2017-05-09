739a910d5d422bdd6d109d0e860fbaf0bb76e072
Joachim Andersen <joachimdamandersen@gmail.com>
2017-05-09 23:10:32 +0200

    implemented toobox for SHA384

--- Hashify/Hashify/Crypto/HashFunctions/Sha384ToolBox.cs
+++ Hashify/Hashify/Crypto/HashFunctions/Sha384ToolBox.cs
@@ -1,17 +1,17 @@
-﻿using System;
+using System;
 using System.IO;
 
 namespace Hashify.Crypto.HashFunctions
 {
-    public class Sha256ToolBox
+    public class Sha384ToolBox
     {
         public static string GetFileHashAsString(string fileName, Func<byte[], string> bytesToHex)
         {
-            using (var sha2 = System.Security.Cryptography.SHA256.Create())
+            using (var sha3 = System.Security.Cryptography.SHA384.Create())
             {
                 using (var stream = File.OpenRead(fileName))
                 {
-                    var hashBytes = sha2.ComputeHash(stream);
+                    var hashBytes = sha3.ComputeHash(stream);
                     return bytesToHex(hashBytes);
                 }
             }
