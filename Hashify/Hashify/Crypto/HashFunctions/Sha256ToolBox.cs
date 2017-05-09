22a3806cd23bccba2aa9100a630c8615d74ea4a1
Joachim Andersen <joachimdamandersen@gmail.com>
2017-05-09 23:07:36 +0200

    implemented toolbox for SHA256 hasing

---
+++ Hashify/Hashify/Crypto/HashFunctions/Sha256ToolBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashify.Crypto.HashFunctions
+{
+    public class Sha256ToolBox
+    {
+        public static string GetFileHashAsString(string fileName, Func<byte[], string> bytesToHex)
+        {
+            using (var sha2 = System.Security.Cryptography.SHA256.Create())
+            {
+                using (var stream = File.OpenRead(fileName))
+                {
+                    var hashBytes = sha2.ComputeHash(stream);
+                    return bytesToHex(hashBytes);
+                }
+            }
+        }
+    }
+}
